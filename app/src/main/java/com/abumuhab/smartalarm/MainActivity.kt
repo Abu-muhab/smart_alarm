01b7a8066bb04c4f74adc531a489fd6009775826
Abdulmalik <abumuhab98@gmail.com>
2021-10-29 23:44:48 +0100

    squashed some bugs

--- app/src/main/java/com/abumuhab/smartalarm/MainActivity.kt
+++ app/src/main/java/com/abumuhab/smartalarm/MainActivity.kt
@@ -2,9 +2,11 @@ package com.abumuhab.smartalarm
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
